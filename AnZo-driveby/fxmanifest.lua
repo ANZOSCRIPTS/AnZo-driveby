0d326c4dd8f15d64e33ec036aa0fe67e6445aab2
AnZoFPS <rajuverma4959@gmail.com>
2025-07-10 22:03:39 +0530

    Add files via upload

---
+++ AnZo-driveby/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'AnZoScripts'
+Version '1.0.1'
+description 'Configurable Driveby Script (Foot. Car. Heli) for QBX'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
